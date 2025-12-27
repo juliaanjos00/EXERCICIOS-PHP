b6bd20f2054149f98fab52f51b49bfe9ae3928bd
juliaanjos0 <anjosjulianjos09@gmail.com>
2025-12-26 23:01:32 -0300

    Envio do exercicio 1 com pull request

---
+++ Exercicio-1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EXERCICIO 1</title>
+</head>
+ <body>
+  <form method= "post" action= "">
+   <div class= box>
+
+    <h1>Exercício 1</h1>    
+    <h2>Monitor de Temperatura</h2>
+  
+    <div class= inputs>
+
+      <label for= "manha"> Temperatura da Manhã °C: </label>
+      <input type= "number" id= "manha" name= "input">
+
+      <label for= "tarde"> Temperatura da Tarde °C: </label>
+      <input type= "number" id= "tarde" name= "input">
+
+    </div>
+
+   <button class= "button"> Calcular </button>
+  
+  </div>
+
+  </form>
+
+ </body>
+</html>
