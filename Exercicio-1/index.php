993e4feb996fff463b735c4b9b37cf9f9d9dd20a
juliaanjos0 <anjosjulianjos09@gmail.com>
2025-12-27 19:49:22 -0300

    envio do backend do exercicio 1

--- Exercicio-1/index.php
+++ Exercicio-1/index.php
@@ -1,33 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>EXERCICIO 1</title>
 </head>
- <body>
-  <form method= "post" action= "">
-   <div class= box>
+<body>
 
-    <h1>Exercício 1</h1>    
-    <h2>Monitor de Temperatura</h2>
-  
-    <div class= inputs>
+<form method="post">
+    <div class="box">
 
-      <label for= "manha"> Temperatura da Manhã °C: </label>
-      <input type= "number" id= "manha" name= "input">
+        <h1>Exercício 1</h1>    
+        <h2>Monitor de Temperatura</h2>
+      
+         <div class="inputs">
 
-      <label for= "tarde"> Temperatura da Tarde °C: </label>
-      <input type= "number" id= "tarde" name= "input">
+              <label for="manha"> Temperatura da Manhã °C: </label>
+              <input type="number" id="manha" name="manha">
+
+              <label for="tarde"> Temperatura da Tarde °C: </label>
+              <input type="number" id="tarde" name="tarde">
+
+         </div>
+
+          <button class="button"> Calcular </button>
+
+         <?php
+          if (($_POST)) {
+
+              $manha = $_POST['manha'];
+              $tarde = $_POST['tarde'];
+
+              $media = ($manha + $tarde) / 2;
+
+              if ($media > 25) {
+                  $media = $media + 3;
+              } else {
+                  $media = $media - 2;
+              }
+
+              echo "<p><strong> A temperatura final é de: $media °C</strong></p>";
+          }
+        ?>
 
     </div>
+</form>
 
-   <button class= "button"> Calcular </button>
-  
-  </div>
-
-  </form>
-
- </body>
+</body>
 </html>
