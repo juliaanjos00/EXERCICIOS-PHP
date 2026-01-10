42ac548588ad3f719c8727871cd598005f0726f8
juliaanjos0 <anjosjulianjos09@gmail.com>
2026-01-10 00:11:26 -0300

    envio do frontend do exercicio 2

---
+++ Exercicio-2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EXERCICIO 2</title>
+</head>
+<body>
+
+<form method="post">
+    <div class="box">
+
+        <h1>Exercício 2</h1>    
+        <h2>Múltiplos de 8, 5 e 3</h2>
+      
+        <div class="inputs">
+            <label for="number">Digite um número:</label>
+            <input type="number" id="number" name="number">
+        </div>
+
+        <button class="button">Verificar</button>
+
+        <p class="resultado">
+        </p>
+
+    </div>
+</form>
+
+</body>
+</html>
+
+
+              
