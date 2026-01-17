5e4920c41226edae9ffd1f4ea4a88887bafee418
juliaanjos0 <anjosjulianjos09@gmail.com>
2026-01-17 04:51:26 -0300

    adicionado até o exercício 20 no índice

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Indice</title>
 </head>
 <body>
+
+  <class= list>
   <h1>Índice</h1>
      <ul>
-      <li> <a href="/Exercicio-1"> Exercicio 1</a></li>
+       <li> <a href="/Exercicio-1"> Exercicio 1</a></li>
+       <li> <a href="/Exercicio-2"> Exercício 2</a></li>
+       <li> <a href="/Exercicio-3"> Exercício 3</a></li>
+       <li> <a href="/Exercicio-4"> Exercício 4</a></li>
+       <li> <a href="/Exercicio-5"> Exercício 5</a></li>
+       <li> <a href="/Exercicio-6"> Exercício 6</a></li>
+       <li> <a href="/Exercicio-7"> Exercício 7</a></li>
+       <li> <a href="/Exercicio-8"> Exercício 8</a></li>
+       <li> <a href="/Exercicio-9"> Exercício 9</a></li>
+       <li> <a href="/Exercicio-10"> Exercício 10</a></li>
+       <li> <a href="/Exercicio-11"> Exercício 11</a></li>
+       <li> <a href="/Exercicio-12"> Exercício 12</a></li>
+       <li> <a href="/Exercicio-13"> Exercício 13</a></li>
+       <li> <a href="/Exercicio-14"> Exercício 14</a></li>
+       <li> <a href="/Exercicio-15"> Exercício 15</a></li>
+       <li> <a href="/Exercicio-16"> Exercício 16</a></li>
+       <li> <a href="/Exercicio-17"> Exercício 17 </a></li>
+       <li> <a href="/Exercicio-18"> Exercício 18</a></li>
+       <li> <a href="/Exercicio-19"> Exercício 19</a></li>
+       <li> <a href="/Exercicio-20"> Exercício 20</a></li>
     </ul>
 </body>
 </html>
