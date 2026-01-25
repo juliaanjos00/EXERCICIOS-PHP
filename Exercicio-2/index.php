efc0944819bade5227a95d2069142ce7d3ebb15c
juliaanjos0 <anjosjulianjos09@gmail.com>
2026-01-25 02:41:07 -0300

    adicionado o backend do exercicio2

--- Exercicio-2/index.php
+++ Exercicio-2/index.php
@@ -22,8 +22,33 @@
         <button class="button">Verificar</button>
 
         <p class="resultado">
-        </p>
+            <?php
 
+              if (isset($_POST["number"])) {
+                $numero = $_POST["number"];
+
+                if ($numero % 8 == 0 && $numero % 5 == 0 && $numero % 3 == 0) {
+                     echo "Múltiplo de 8, 5 e 3";
+                 } elseif ($numero % 8 == 0 && $numero % 5 == 0) {
+                     echo "Múltiplo de 8 e 5";
+                 } elseif ($numero % 8 == 0 && $numero % 3 == 0) {
+                     echo "Múltiplo de 8 e 3";
+                 } elseif ($numero % 5 == 0 && $numero % 3 == 0) {
+                     echo "Múltiplo de 5 e 3";
+                 } elseif ($numero % 8 == 0) {
+                     echo "Múltiplo de 8";
+                 } elseif ($numero % 5 == 0) {
+                     echo "Múltiplo de 5";
+                 } elseif ($numero % 3 == 0) {
+                     echo "Múltiplo de 3";
+                 } else {
+                     echo "Não é múltiplo de 8, 5 ou 3";
+                }
+              }
+             
+            ?>
+            
+        </p>
     </div>
 </form>
 
