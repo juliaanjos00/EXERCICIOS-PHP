487d7bc431f5906aea8f0ed77ee2a2004b579367
juliaanjos0 <anjosjulianjos09@gmail.com>
2026-02-07 23:10:00 -0300

    frontend do exercicío 3

---
+++ Exercicio-3/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EXERCICIO 3</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EXERCICIO 3</title>
+</head>
+<body>
+
+<form method="post">
+    <div class="box">
+
+        <h1>Exercício 3</h1>
+        <h2>Empréstimo de Equipamentos</h2>
+
+        <div class="inputs">
+            <label>Nome completo:</label>
+            <input type="text" name="nome">
+        </div>
+
+        <div class="inputs">
+            <label>Status:</label>
+            <select name="status">
+                <option value="ativo">Sócio Ativo</option>
+                <option value="inadimplente">Sócio Inadimplente</option>
+            </select>
+        </div>
+
+        <div class="inputs">
+            <label> Quantidade de equipamentos emprestados:</label>
+            <input type="number" name="quantidade">
+        </div>
+
+         <button class="button">Verificar</button>
+         <p class="resultado">  
+
+    </div>
+</form>
+
+</body>
+</html>
