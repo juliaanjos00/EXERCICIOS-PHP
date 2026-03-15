45af4c6ca6c7fdf18741a5cc26f85d9a8b96000d
juliaanjos0 <anjosjulianjos09@gmail.com>
2026-03-15 00:51:27 -0300

    backend do exercicio 4

--- Exercicio-4/index.php
+++ Exercicio-4/index.php
@@ -32,6 +32,67 @@
 
             <div class="resultado">
 
+             <?php
+
+              if($_POST){
+
+                 $r1 = $_POST["receita1"];
+                 $r2 = $_POST["receita2"];
+                 $r3 = $_POST["receita3"];
+
+                 $c1 = $_POST["curtidas1"];
+                 $c2 = $_POST["curtidas2"];
+                 $c3 = $_POST["curtidas3"];
+
+                 $total = $c1 + $c2 + $c3;
+
+                 echo "<p>🍽️ Ranking de curtidas: 🍽️</p>";
+
+                 if($c1 > $c2 && $c1 > $c3){
+
+                    echo "1º - $r1: $c1 curtidas <br>";
+
+                    if($c2 > $c3){
+                        echo "2º - $r2: $c2 curtidas <br>";
+                        echo "3º - $r3: $c3 curtidas <br>";
+                    } else {
+                        echo "2º - $r3: $c3 curtidas <br>";
+                        echo "3º - $r2: $c2 curtidas <br>";
+                         }
+
+                }    else if($c2 > $c1 && $c2 > $c3){
+
+                     echo "1º - $r2: $c2 curtidas <br>";
+
+                     if($c1 > $c3){
+                        echo "2º - $r1: $c1 curtidas <br>";
+                        echo "3º - $r3: $c3 curtidas <br>";
+                     } else {
+                        echo "2º - $r3: $c3 curtidas <br>";
+                        echo "3º - $r1: $c1 curtidas <br>";
+                        }
+
+                 } else {
+
+                    echo "1º - $r3: $c3 curtidas <br>";
+
+                     if($c1 > $c2){
+                        echo "2º - $r1: $c1 curtidas <br>";
+                        echo "3º - $r2: $c2 curtidas <br>";
+                     } else {
+                        echo "2º - $r2: $c2 curtidas <br>";
+                        echo "3º - $r1: $c1 curtidas <br>";
+                        }
+
+                }
+
+                 echo "<br>Total de curtidas das receitas da Júlia: $total";
+
+             }
+
+            ?>
+
+
             </div>
         </div>
     </form>
