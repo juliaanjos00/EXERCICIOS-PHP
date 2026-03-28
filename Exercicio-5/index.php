ccb64f7d70dc8ef226db11a5571ff2fd20ca5dd5
juliaanjos0 <anjosjulianjos09@gmail.com>
2026-03-28 04:08:43 -0300

    Envio do frontend do exercício 5

---
+++ Exercicio-5/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+ <body>
+
+    <form method="post">
+        <div class="box">
+            <h1>Sistema De Classificação de Receita</h1>
+
+            <div class="inputs">
+
+                <label for="tempo">Tempo de preparo em minutos:</label>
+                <input type="number" name="tempo" id="tempo" placeholder="Digite o tempo">
+
+                <label for="ingredientes">Número de ingredientes:</label>
+                <input type="number" name="ingredientes" id="ingredientes" placeholder="Digite a quantidade">
+
+                <label for="etapas">Número de etapas:</label>
+                <input type="number" name="etapas" id="etapas" placeholder="Digite as etapas">
+
+            </div>
+
+            <button class="button">Classificar</button>
+
+            <div class="resultado">
+
+            </div>
+
+            <div class="resultado">
+            
+            </div>
+            
+        </div>
+    </form>
+
+ </body>
+</html>
